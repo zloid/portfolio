26fd351be20e9a6a077cb22a819bced8d48cddb5
zloid <zloid-1@yandex.by>
2021-02-07 19:13:41 +0100

    fix: projectsSlice

--- src/features/projects/projectsSlice.js
+++ src/features/projects/projectsSlice.js
@@ -1,19 +1,92 @@
-import { normalize, schema } from "normalizr";
- 
+import { normalize, schema } from 'normalizr'
+
 const initialState = {
-   "results" : [
-     {id: '1', res: 'one'},
-     {id: '2', res: 'two'},
-     {id: '3', res: 'three'},
-   ]
+    articles: [
+        {
+            id: 1,
+            title: 'Dagon',
+            tags: [
+                { id: 1, name: 'old ones' },
+                { id: 2, name: 'two' },
+                { id: 3, name: 'three' },
+                { id: 5, name: 'four' },
+                { id: 666, name: 'devil' },
+            ],
+        },
+        {
+            id: 2,
+            title: 'Azathoth',
+            tags: [
+                { id: 1, name: 'old ones' },
+                { id: 4, name: 'four' },
+                { id: 666, name: 'devil' },
+            ],
+        },
+        {
+            id: 3,
+            title: 'At the Mountains of Madness',
+            tags: [
+                { id: 4, name: 'insanity' },
+                { id: 666, name: 'devil' },
+            ],
+        },
+    ],
 }
+const tag = new schema.Entity('tags', {})
+const article = new schema.Entity('articles', {
+    tags: [tag],
+})
 
-const project = new schema.Entity('someEntity')
+const resultEntyty = { articles: [article] }
 
-const resultEntyty = new schema.Entity('resultEntyty')
- 
 const normalizedState = normalize(initialState, resultEntyty)
 
 export default function projectsReducer(state = normalizedState) {
     return state
 }
+
+const ob = {
+    entities: {
+        tags: {
+            1: {
+                id: 1,
+                name: 'old ones',
+            },
+            2: {
+                id: 2,
+                name: 'short story',
+            },
+            3: {
+                id: 3,
+                name: 'novel',
+            },
+            4: {
+                id: 4,
+                name: 'insanity',
+            },
+        },
+        articles: {
+            1: {
+                id: 1,
+                title: 'Dagon',
+                tags: [1, 2],
+            },
+            2: {
+                id: 2,
+                title: 'Azathoth',
+                tags: [1, 3],
+            },
+            3: {
+                id: 3,
+                title: 'At the Mountains of Madness',
+                tags: [4, 3],
+            },
+        },
+    },
+    result: {
+        articles: [1, 2, 3],
+    },
+}
+
+// console.log(ob.entities.articles[1])
+// ob.result.articles.map(e => console.log(ob.entities.articles[e]))
